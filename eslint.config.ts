8b3ae19ba69bb48aba2b6340d68e54deac1c4036
Phil <ijeffrouk@gmail.com>
2025-12-02 19:51:31 +0000

    Update README files

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,32 +6,35 @@ import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
-    {
-        files: ["*.ts", "*.tsx"],
-        languageOptions: {
-            parser: tsParser, // <-- use the parser object, not a string
-            parserOptions: {
-                ecmaVersion: "latest",
-                sourceType: "module",
-                ecmaFeatures: { jsx: true },
-            },
-            globals: {
-                React: "readonly",
-            },
-        },
-        plugins: {
-            "@typescript-eslint": tsPlugin,
-            react: reactPlugin,
-            "react-hooks": reactHooksPlugin,
-            "jsx-a11y": jsxA11yPlugin,
-            prettier: prettierPlugin,
-        },
-        rules: {
-            "prettier/prettier": "error",
-            "react/react-in-jsx-scope": "off",
-            "react/prop-types": "off",
-            "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-            "@typescript-eslint/consistent-type-imports": "error",
-        },
+  {
+    files: ["*.ts", "*.tsx"],
+    languageOptions: {
+      parser: tsParser, // <-- use the parser object, not a string
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        React: "readonly",
+      },
     },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "jsx-a11y": jsxA11yPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/consistent-type-imports": "error",
+    },
+  },
 ];
