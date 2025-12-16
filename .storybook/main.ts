e37cb55101dd9f8cef2ffc0abe32a14cd52ebb94
Phil <ijeffrouk@gmail.com>
2025-12-16 20:47:33 +0000

    Fix Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,9 +4,14 @@ const config: StorybookConfig = {
   stories: [`../src/**/*.stories.@(js|jsx|mjs|ts|tsx)`],
   addons: [
     `@storybook/addon-docs`,
+    `@storybook/addon-a11y`,
     `@storybook/addon-vitest`,
     `@storybook-community/storybook-dark-mode`,
   ],
   framework: `@storybook/react-vite`,
+  // viteFinal: async (config) => {
+  //   config.base = "/leo/";
+  //   return config;
+  // },
 };
 export default config;
